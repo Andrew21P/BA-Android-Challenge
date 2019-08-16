2a855749f5b0a5b87e40207df5c57c9a2274a33e
Andrew Fernandes <andrew.fernandes@inm.pt>
2019-08-16 17:11:04 +0100

    Resource files

---
+++ app/src/main/java/pt/andrew/blisschallenge/screens/QuestionsScreenActivity.java
@@ -0,0 +1,15 @@
+package pt.andrew.blisschallenge.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pt.andrew.blisschallenge.R;
+
+public class QuestionsScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_screen);
+    }
+}
